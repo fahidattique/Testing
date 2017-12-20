bb42395f9c14bd5703bc22ce86c75acaf531696b
Fahid Attique <fahidattique@fahid-attique.local>
2017-12-20 12:05:18 +0500

    "fixes #1"

--- TeStInG/ViewController.swift
+++ TeStInG/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     }
 
     override func didReceiveMemoryWarning() {
+        
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
